4673938a884da7d73bba116965b8781631c7d171
Aldinonid <aldino.efendi@icloud.com>
2026-05-18 22:19:47 +0700

    Initial Commit

---
+++ TaskExplorer/TaskExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskExplorerApp.swift
+//  TaskExplorer
+//
+//  Created by Aldino Efendi on 2026/05/18.
+//
+
+import SwiftUI
+
+@main
+struct TaskExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
